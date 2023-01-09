9a65528565577f0f2c7be482de32b6cc9ac3b10e
Bananarama92 <105885570+bananarama92@users.noreply.github.com>
2023-01-09 17:54:47 +0100

    f

--- src/loader.user.js
+++ src/loader.user.js
@@ -19,7 +19,7 @@ setTimeout(
         const n = document.createElement("script");
         n.language = "JavaScript";
         n.crossorigin = "anonymous";
-        n.src = "https://bananarama92.github.io/MBS/mbs.js";
+        n.src = "https://bananarama92.github.io/MBS/0.1.0/mbs.js";
         document.head.appendChild(n);
     },
     2000,
