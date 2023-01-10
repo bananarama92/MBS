e1b94c0a29ff5b7827b4bd9068be8357b0cc05b6
Bananarama92 <105885570+bananarama92@users.noreply.github.com>
2023-01-10 20:06:03 +0100

    f

--- src/dev_loader.user.js
+++ src/dev_loader.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         MBS - Maid's Bondage Scripts
 // @namespace    MBS
-// @version      0.1.0
+// @version      dev0
 // @description  Loader of Bananarama92's "Maid's Bondage Scripts" mod
 // @author       Bananarama92
 // @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @homepage     https://github.com/bananarama92/MBS.git#readme
 // @source       https://github.com/bananarama92/MBS.git
-// @downloadURL  https://github.com/bananarama92/MBS/src/loader.user.js
+// @downloadURL  https://github.com/bananarama92/MBS/src/dev_loader.user.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
@@ -19,7 +19,7 @@ setTimeout(
         const n = document.createElement("script");
         n.language = "JavaScript";
         n.crossorigin = "anonymous";
-        n.src = "https://bananarama92.github.io/MBS/0.1.0/mbs.js";
+        n.src = "https://bananarama92.github.io/MBS/dev0/mbs.js";
         document.head.appendChild(n);
     },
     2000,
