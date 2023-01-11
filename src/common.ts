c3b54fc84513f51bbab59addad15f55fad07ea83
Bananarama <105885570+bananarama92@users.noreply.github.com>
2023-01-11 13:40:23 +0100

    REL: Version bump: 0.1.8 > 0.1.9

--- src/common.ts
+++ src/common.ts
@@ -96,7 +96,7 @@ export function getVersions(): Record<string, string> {
 }
 
 /** The MBS version. */
-export const MBS_VERSION = "0.1.8";
+export const MBS_VERSION = "0.1.9";
 
 /** The MBS {@link ModSDKGlobalAPI} instance. */
 export const MBS_MOD_API = bcModSdk.registerMod({
