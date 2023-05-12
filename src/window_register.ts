40d2f88d179b603ea9bc5c608fa85da7d83d2e53
bananarama92 <bananarama921@outlook.com>
2023-05-12 18:39:28 +0200

    MAINT: Reduce the number of exposed functions

--- src/window_register.ts
+++ src/window_register.ts
@@ -4,51 +4,16 @@
 
 import { waitFor } from "common";
 import { settingsMBSLoaded } from "common_bc";
-
-import * as common from "common";
-import * as common_bc from "common_bc";
-import * as sanity_checks from "sanity_checks";
-import * as crafting from "crafting";
-import * as equipper from "equipper";
-import * as fortune_wheel_item_set from "fortune_wheel_item_set";
-import * as fortune_wheel_command from "fortune_wheel_command";
-import * as fortune_wheel_select from "fortune_wheel_select";
-import * as fortune_wheel from "fortune_wheel";
-import * as item_bundle from "item_bundle";
-import * as settings from "settings";
-import * as screen_abc from "screen_abc";
-import * as testing from "testing";
-import * as backport from "backport";
-
 import { FWItemSetScreen } from "fortune_wheel_item_set";
 import { FWCommandScreen } from "fortune_wheel_command";
 import { FWSelectScreen} from "fortune_wheel_select";
 
 waitFor(settingsMBSLoaded).then(() => {
-    const exportMBS = Object.freeze({
-        common: Object.freeze(common),
-        common_bc: Object.freeze(common_bc),
-        crafting: Object.freeze(crafting),
-        equipper: Object.freeze(equipper),
-        fortune_wheel_item_set: Object.freeze(fortune_wheel_item_set),
-        fortune_wheel_command: Object.freeze(fortune_wheel_command),
-        fortune_wheel_select: Object.freeze(fortune_wheel_select),
-        fortune_wheel: Object.freeze(fortune_wheel),
-        item_bundle: Object.freeze(item_bundle),
-        settings: Object.freeze(settings),
-        screen_abc: Object.freeze(screen_abc),
-        sanity_checks: Object.freeze(sanity_checks),
-        testing: Object.freeze(testing),
-        port_forward: Object.freeze(backport),
-        runTests: testing.runTests,
-        MBS_VERSION: common.MBS_VERSION,
-        MBS_MOD_API: common.MBS_MOD_API,
-    });
     const backgrounds = {
-        [`${FWItemSetScreen.screen}Background`]: `${FWItemSetScreen.background}`,
-        [`${FWCommandScreen.screen}Background`]: `${FWCommandScreen.background}`,
-        [`${FWSelectScreen.screen}Background`]: `${FWSelectScreen.background}`,
-    };
-    const w = <typeof window & typeof exportMBS & typeof backgrounds>window;
-    Object.assign(w, backgrounds, { MBS: exportMBS });
+        [`${FWItemSetScreen.screen}Background`]: FWItemSetScreen.background,
+        [`${FWCommandScreen.screen}Background`]: FWCommandScreen.background,
+        [`${FWSelectScreen.screen}Background`]: FWSelectScreen.background,
+    } as const;
+    const w = <typeof window & typeof backgrounds>window;
+    Object.assign(w, backgrounds);
 });
