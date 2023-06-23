22d740adb5b8a477634d06dec052747013f38ab2
bananarama92 <bananarama921@outlook.com>
2023-06-23 17:47:57 +0200

    MAINT: Partially backport BondageProjects/Bondage-College#4332
    * https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/4332 - Rename the LuckyWheel functions to use the key

--- src/backport.ts
+++ src/backport.ts
@@ -7,6 +7,26 @@ import { settingsMBSLoaded } from "common_bc";
 export const backportIDs: Set<number> = new Set();
 
 waitFor(settingsMBSLoaded).then(() => {
+    if (GameVersion === "R93") {
+        const w: typeof window & Record<string, any> = window;
+        if (typeof InventoryItemDevicesLuckyWheelGame0Load !== "undefined") {
+            w["InventoryItemDevicesLuckyWheelg0Load"] = InventoryItemDevicesLuckyWheelGame0Load;
+            backportIDs.add(4332);
+        }
+        if (typeof InventoryItemDevicesLuckyWheelGame0Draw !== "undefined") {
+            w["InventoryItemDevicesLuckyWheelg0Draw"] = InventoryItemDevicesLuckyWheelGame0Draw;
+            backportIDs.add(4332);
+        }
+        if (typeof InventoryItemDevicesLuckyWheelGame0Click !== "undefined") {
+            w["InventoryItemDevicesLuckyWheelg0Click"] = InventoryItemDevicesLuckyWheelGame0Click;
+            backportIDs.add(4332);
+        }
+        if (typeof InventoryItemDevicesLuckyWheelGame0Exit !== "undefined") {
+            w["InventoryItemDevicesLuckyWheelg0Exit"] = InventoryItemDevicesLuckyWheelGame0Exit;
+            backportIDs.add(4332);
+        }
+    }
+
     if (backportIDs.size) {
         console.log("MBS: Initializing R94 bug fix backports", backportIDs);
     } else {
