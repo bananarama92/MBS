0626ec109a6da80d17426938b5ea157089316190
bananarama92 <bananarama921@outlook.com>
2023-11-27 17:20:27 +0100

    BLD: Make the MBS version/git tag comparison more robust

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -13,6 +13,37 @@ import packageJson from "./package.json" assert { type: "json" };
 
 /* global process */
 
+/**
+ * Increment the package version if it is lower than or equal to the latest git tag.
+ * @param {string} pkgVersion
+ * @param {string} gitVersion
+ * @returns {string}
+ */
+function incrementVersion(pkgVersion, gitVersion) {
+    const version_pattern = /^(v?)([0-9]+)\.([0-9]+)\.([0-9]+)$/;
+    const gitExec = version_pattern.exec(gitVersion);
+    const pkgExec = version_pattern.exec(pkgVersion);
+    if (gitExec === null || pkgExec === null) {
+        const version = gitExec === null ? gitVersion : pkgVersion;
+        console.error(`MBS: Failed to parse version "${version}"`);
+        return pkgVersion;
+    }
+
+    const gitArray = gitExec.slice(2, 5).map(i => Number.parseInt(i));
+    const pkgArray = pkgExec.slice(2, 5).map(i => Number.parseInt(i));
+    for (let i=0; i < 3; i++) {
+        if (pkgArray[i] > gitArray[i]) {
+            return pkgVersion;
+        } else if (pkgArray[i] !== gitArray[i]) {
+            break;
+        }
+    }
+
+    // Automatically increment the micro version by 1 w.r.t. the latest release if we haven't done so manually
+    gitArray[2] += 1;
+    return gitArray.join(".");
+}
+
 /** @type {import("rollup").RollupOptions} */
 const config = {
     input: "src/index.ts",
@@ -36,21 +67,11 @@ const config = {
                 if (commits.length === 0) {
                     console.log(`MBS: No new commits since the "${latestTag}" tag`);
                     break devsuffix;
+                } else {
+                    version = incrementVersion(version, latestTag);
                 }
 
-                // Automatically increment the micro version by 1 w.r.t. the latest release if we haven't done so manually
-                if (latestTag === `v${version}`) {
-                    const versionSplit = version.split(".");
-                    const micro = Number.parseInt(versionSplit[2]) + 1;
-                    if (Number.isNaN(micro)) {
-                        console.error(`MBS: Failed to parse the "${version}" micro version, invalid integer type`);
-                        break devsuffix;
-                    }
-                    versionSplit[2] = micro.toString();
-                    version = versionSplit.join(".");
-                }
-
-                const hash = commits.at(-1).hash;
+                const hash = commits[0].hash;
                 version += `.dev${commits.length - 1}+${hash.slice(0, 8)}`;
                 console.log(`MBS: Updated version "${packageJson.version}" to "${version}"`);
             }
