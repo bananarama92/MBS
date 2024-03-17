5838406cb86bd2890fee8badc784e2f0fd9aea5f
bananarama92 <bananarama921@outlook.com>
2024-03-17 22:55:03 +0100

    MAINT: Backport one R103 PR
    * [BondageProjects/Bondage-College$4900](https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/4900): Optimize the handling of crafted items in `DialogInventoryBuild`

--- src/backport.ts
+++ src/backport.ts
@@ -14,9 +14,65 @@ const BC_NEXT = BC_MIN_VERSION + 1;
 /** A set with the pull request IDs of all applied bug fix backports */
 export const backportIDs: Set<number> = new Set();
 
+declare const CraftingAssetsPopulate: undefined | (() => void);
+
+/**
+ * Construct a record mapping all crafting-valid asset names to a list of matching elligble assets.
+ * Elligble assets are defined as crafting-valid assets with either a matching {@link Asset.Name} or {@link Asset.CraftGroup}.
+ */
+function craftingAssetsPopulate(): Record<string, readonly Asset[]> {
+    const ret: Record<string, Asset[]> = {};
+    const craftGroups: Record<string, Asset[]> = {};
+    for (const a of Asset) {
+        if (!a.Group.IsItem() || a.IsLock || !a.Wear || !a.Enable) {
+            continue;
+        } else if (a.CraftGroup) {
+            craftGroups[a.CraftGroup] ??= [];
+            craftGroups[a.CraftGroup].push(a);
+        } else {
+            ret[a.Name] ??= [];
+            ret[a.Name].push(a);
+        }
+    }
+
+    for (const assetList of Object.values(craftGroups)) {
+        const names = new Set(assetList.map(a => a.Name));
+        for (const name of names) {
+            ret[name] ??= [];
+            ret[name].push(...assetList);
+        }
+    }
+    return ret;
+}
+
 waitFor(bcLoaded).then(() => {
     switch (GameVersion) {
         case "R102": {
+            if (typeof CraftingAssetsPopulate === "undefined") {
+                backportIDs.add(4900);
+                const CRAFTING_ASSETS = Object.freeze(craftingAssetsPopulate());
+
+                MBS_MOD_API.hookFunction("CraftingAppliesToItem", 10, (args) => {
+                    const [craft, asset] = args as Parameters<typeof CraftingAppliesToItem>;
+                    if (!craft || !asset) {
+                        return false;
+                    } else {
+                        const elligbleAssets = CRAFTING_ASSETS[craft.Item] ?? [];
+                        return elligbleAssets.includes(asset);
+                    }
+                });
+
+                MBS_MOD_API.hookFunction("DialogCanUseCraftedItem", 10, (args) => {
+                    const [character, craft] = args as Parameters<typeof DialogCanUseCraftedItem>;
+                    const elligbleAssets = CRAFTING_ASSETS[craft.Item] ?? [];
+                    return elligbleAssets.some(a => {
+                        if (a.OwnerOnly && !character.IsOwnedByPlayer()) return false;
+                        if (a.LoverOnly && !(character.IsOwnedByPlayer() || character.IsLoverOfPlayer())) return false;
+                        if (a.FamilyOnly && !(character.IsOwnedByPlayer() || character.IsLoverOfPlayer() || character.IsFamilyOfPlayer())) return false;
+                        return true;
+                    });
+                });
+            }
             break;
         }
     }
