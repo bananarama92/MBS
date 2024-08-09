83ae9fe93db040005f316726bd19ae3d0e44de1b
bananarama92 <bananarama921@outlook.com>
2024-08-09 20:25:55 +0200

    REL: Version bump: 1.7.19 > 1.7.20

--- loader.user.js
+++ loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MBS - Maid's Bondage Scripts
 // @namespace    MBS
-// @version     1.7.19
+// @version     1.7.20
 // @description  Loader of Bananarama92's "Maid's Bondage Scripts" mod
 // @author       Bananarama92
 // @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
