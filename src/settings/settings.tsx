7d85a04a26537f0a41761ef8b7510280d03f7c88
bananarama92 <bananarama921@outlook.com>
2024-11-22 14:11:55 +0100

    STY: Misc object-key sorting

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -184,26 +184,27 @@ export function unpackSettings(
 /** Parsed the passed protosettings */
 function parseProtoSettings(s: MBSProtoSettings): SettingsStatus.Base {
     const err: Required<SettingsStatus.Base["err"]> = {
-        Version: [],
+        AlternativeGarbling: [],
         CraftingCache: [],
-        FortuneWheelItemSets: [],
+        DropTrailing: [],
         FortuneWheelCommands: [],
+        FortuneWheelItemSets: [],
         FortuneWheelPresets: [],
         LockedWhenRestrained: [],
-        RollWhenRestrained: [],
-        AlternativeGarbling: [],
-        DropTrailing: [],
         GarblePerSyllable: [],
+        RollWhenRestrained: [],
         ShowChangelog: [],
+        Version: [],
     };
 
     const scalars = {
         CraftingCache: "",
-        LockedWhenRestrained: false,
-        RollWhenRestrained: true,
+
         AlternativeGarbling: false,
         DropTrailing: false,
         GarblePerSyllable: false,
+        LockedWhenRestrained: false,
+        RollWhenRestrained: true,
         ShowChangelog: true,
     } satisfies { [k in keyof typeof err]?: number | boolean | string };
 
@@ -218,17 +219,17 @@ function parseProtoSettings(s: MBSProtoSettings): SettingsStatus.Base {
     }
 
     const settings: MBSSettings = {
-        Version: MBS_VERSION,
+        AlternativeGarbling: scalars.AlternativeGarbling,
         CraftingCache: scalars.CraftingCache,
-        FortuneWheelItemSets: parseFWObjects(FWItemSet.fromObject, s.FortuneWheelSets ?? s.FortuneWheelItemSets ?? [], err.FortuneWheelItemSets),
+        DropTrailing: scalars.DropTrailing,
         FortuneWheelCommands: parseFWObjects(FWCommand.fromObject, s.FortuneWheelCommands ?? [], err.FortuneWheelCommands),
+        FortuneWheelItemSets: parseFWObjects(FWItemSet.fromObject, s.FortuneWheelSets ?? s.FortuneWheelItemSets ?? [], err.FortuneWheelItemSets),
         FortuneWheelPresets: parsePresetArray(s.FortuneWheelPresets ?? [], err.FortuneWheelPresets),
         LockedWhenRestrained: scalars.LockedWhenRestrained,
-        RollWhenRestrained: scalars.RollWhenRestrained,
-        AlternativeGarbling: scalars.AlternativeGarbling,
-        DropTrailing: scalars.DropTrailing,
         GarblePerSyllable: scalars.GarblePerSyllable,
+        RollWhenRestrained: scalars.RollWhenRestrained,
         ShowChangelog: scalars.ShowChangelog,
+        Version: MBS_VERSION,
     };
     if (settings.AlternativeGarbling) {
         garblingJSON.init();
@@ -315,17 +316,17 @@ export function clearMBSSettings(): void {
     WheelFortuneDefault = FORTUNE_WHEEL_DEFAULT_BASE;
     Player.Crafting = Player.Crafting?.slice(0, BC_SLOT_MAX_ORIGINAL);
     Player.MBSSettings = Object.seal({
-        Version: MBS_VERSION,
+        AlternativeGarbling: false,
         CraftingCache: "",
-        FortuneWheelItemSets: Object.seal(Array(MBS_MAX_SETS).fill(null)),
+        DropTrailing: false,
         FortuneWheelCommands: Object.seal(Array(MBS_MAX_SETS).fill(null)),
+        FortuneWheelItemSets: Object.seal(Array(MBS_MAX_SETS).fill(null)),
+        FortuneWheelPresets: Object.seal(Array(MBS_MAX_ID_SETS).fill(null)),
+        GarblePerSyllable: false,
         LockedWhenRestrained: false,
         RollWhenRestrained: true,
-        FortuneWheelPresets: Object.seal(Array(MBS_MAX_ID_SETS).fill(null)),
-        AlternativeGarbling: false,
-        DropTrailing: false,
-        GarblePerSyllable: false,
         ShowChangelog: true,
+        Version: MBS_VERSION,
     });
 
     ServerAccountUpdate.QueueData({
