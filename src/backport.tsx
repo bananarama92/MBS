0abebc2ff0be65814479636ac5043854228b7391
bananarama92 <bananarama921@outlook.com>
2025-02-17 16:44:09 +0100

    ENH: Backport two R113 hotfixes
    * [BondageProjects/Bondage-College#5410](https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/5410): Ensures that updating `C.Stage` and `C.CurrentDialog` triggers an appropriate dialog menu reload
    * [BondageProjects/Bondage-College#5412](https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/5412): Fix `ElementButton` creator function failing to properly expand the `options.icons` array

--- src/backport.tsx
+++ src/backport.tsx
@@ -16,10 +16,79 @@ const BC_NEXT = BC_MIN_VERSION + 1;
 /** A set with the pull request IDs of all applied bug fix backports */
 export const backportIDs: Set<number> = new Set();
 
+type Character2 = Character & { _Stage: string, _CurrentDialog: string };
+
+function updateGetters(char: Character) {
+    if ("_Stage" in char && "_CurrentDialog" in char) {
+        return;
+    }
+
+    const char2 = char as Character2;
+    char2._Stage = char2.Stage;
+    char2._CurrentDialog = char2.CurrentDialog;
+    Object.defineProperty(char2, "Stage", {
+        get(this: Character2) {
+            return this._Stage;
+        },
+        set(this: Character2, value: string) {
+            if (this._Stage === value) {
+                return;
+            }
+            this._Stage = value;
+            if (DialogMenuMode === "dialog") {
+                DialogMenuMapping.dialog.Reload();
+            }
+        },
+    });
+    Object.defineProperty(char2, "CurrentDialog", {
+        get(this: Character2) {
+            return this._CurrentDialog;
+        },
+        set(this: Character2, value: string) {
+            if (this._CurrentDialog === value) {
+                return;
+            }
+            this._CurrentDialog = value;
+            if (DialogMenuMode === "dialog") {
+                DialogSetStatus(value);
+            }
+        },
+    });
+}
+
 waitForBC("backport", {
     async afterLoad() {
         switch (GameVersion) {
             case "R113":
+                if (MBS_MOD_API.getOriginalHash("CharacterCreate") === "F078CBBE") {
+                    backportIDs.add(5410);
+                    MBS_MOD_API.hookFunction("CharacterCreate", 11, (args, next) => {
+                        const char = next(args);
+                        updateGetters(char);
+                        return char;
+                    });
+                    MBS_MOD_API.patchFunction("DialogLoad", {
+                        "C.CurrentDialog = newDialog;":
+                            "C._CurrentDialog = newDialog;",
+                    });
+                    Character.forEach(char => updateGetters(char));
+                }
+
+                if (
+                    MBS_MOD_API.getOriginalHash("ElementButton.CreateForAsset") === "591D7DCF"
+                    && MBS_MOD_API.getOriginalHash("ElementButton.CreateForActivity") === "3A1D0F81"
+                ) {
+                    backportIDs.add(5412);
+                    MBS_MOD_API.patchFunction("ElementButton.CreateForAsset", {
+                        "options.icons ??= [":
+                            "options.icons = [",
+                    });
+                    MBS_MOD_API.patchFunction("ElementButton.CreateForActivity", {
+                        "options.icons ??= [":
+                            "options.icons = [",
+                    });
+                }
+
                 if (!document.getElementById("mbs-backport-style")) {
                     document.body.append(<style id="mbs-backport-style">{styles.toString()}</style>);
                 }
