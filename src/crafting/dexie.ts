15e2be984c257268550675a9f1387a6ff7a7fe0b
bananarama92 <bananarama921@outlook.com>
2025-09-13 13:36:52 +0200

    ENH: Add the ability to store local crafts via indexedDB

---
+++ src/crafting/dexie.ts
@@ -0,0 +1,127 @@
+import { clamp, range } from "lodash";
+import { Dexie, Table, PromiseExtended } from "dexie";
+
+import { Version, logger } from "../common";
+
+export const BC_SLOT_MAX_ORIGINAL = 80;
+export const MBS_SLOT_MAX_SERVER = 160;
+export const MBS_SLOT_MAX_LOCAL = 320;
+const BYTE_CLAMP = 2**8 - 1;
+const SHORT_CLAMP = 2**16 - 1;
+
+interface CraftingDBData {
+    id: number;
+    data: CraftingItem;
+}
+
+/**
+ * Encode the MBS version as a 32-bit unsigned integer
+ * @param version
+ */
+export function encodeVersion(version: Version): number {
+    const view = new DataView(new ArrayBuffer(4), 0);
+    view.setUint8(0, clamp(version.major, 0, BYTE_CLAMP));
+    view.setUint8(1, clamp(version.minor, 0, BYTE_CLAMP));
+    view.setUint16(2, clamp(version.micro, 0, SHORT_CLAMP));
+    return view.getUint32(0);
+}
+
+/**
+ * Decode the MBS version from a 32-bit unsigned integer
+ * @param version
+ */
+export function decodeVersion(version: number): Version {
+    const view = new DataView(new ArrayBuffer(8), 0);
+    view.setUint32(0, version);
+    return new Version(view.getUint8(0), view.getUint8(1), view.getUint16(2));
+}
+
+export function openDB(playerID: number): Dexie {
+    const versionObj = Version.fromVersion(MBS_VERSION);
+    const version = encodeVersion(versionObj);
+    const db = new Dexie(`mbs_${playerID}`);
+    db.version(version).stores({ crafting: "id" });
+    return db;
+}
+
+export async function deleteDB(playerID: number) {
+    logger.debug(`Deleting indexedDB database ${playerID}`);
+    return new Dexie(`mbs_${playerID}`).delete();
+}
+
+export function saveCraft(db: Dexie, index: number, craft: null): PromiseExtended<void>;
+export function saveCraft(db: Dexie, index: number, craft: CraftingItem): PromiseExtended<number>;
+export function saveCraft(db: Dexie, index: number, craft: null | CraftingItem): PromiseExtended<void | number>;
+export function saveCraft(db: Dexie, index: number, craft: null | CraftingItem): PromiseExtended<void | number> {
+    const table: Table<CraftingDBData, number, CraftingDBData> = db.table("crafting");
+    if (craft) {
+        logger.debug(`Saving craft ${index + MBS_SLOT_MAX_SERVER} to indexedDB: ${craft.Name} (${craft.Item})`);
+        return table.put({ id: index, data: craft }, index);
+    } else {
+        logger.debug(`Deleting craft ${index + MBS_SLOT_MAX_SERVER} from indexedDB`);
+        return table.delete(index);
+    }
+}
+
+export async function saveAllCraft(db: Dexie, crafts: readonly (null | CraftingItem)[]) {
+    const craftStrings = crafts.map((data, id) => data != null ? { id, data } : data);
+    const putIndices = [];
+    const deleteIndices = [];
+    for (const [i, craftString] of craftStrings.entries()) {
+        if (craftString == null) {
+            deleteIndices.push(i);
+        } else {
+            putIndices.push(i);
+        }
+    }
+    logger.debug(`Saving all ${putIndices.length} crafts to indexedDB`);
+
+    const table: Table<CraftingDBData, number, CraftingDBData> = db.table("crafting");
+    table.bulkPut(craftStrings.filter(i => i != null), putIndices);
+    return Promise.all([
+        table.bulkPut(craftStrings.filter(i => i != null), putIndices),
+        table.bulkDelete(deleteIndices),
+    ]);
+}
+
+export function deleteCraft(db: Dexie, index: number) {
+    const table: Table<CraftingDBData, number, CraftingDBData> = db.table("crafting");
+    return table.delete(index);
+}
+
+function craftingValidate(craft: unknown, index: number) {
+    if (!CommonIsObject(craft)) {
+        return null;
+    }
+
+    const craftCandidate = craft as unknown as CraftingItem;
+    switch (CraftingValidate(craftCandidate)) {
+        case CraftingStatusType.OK:
+        case CraftingStatusType.ERROR:
+            return craftCandidate;
+        case CraftingStatusType.CRITICAL_ERROR:
+            logger.error(`Removing corrupt crafting item ${index}: "${craftCandidate?.Name} (${craftCandidate?.Item})"`);
+            return null;
+    }
+}
+
+export async function loadCraft(db: Dexie, index: number): Promise<null | CraftingItem> {
+    const table: Table<CraftingDBData, number, CraftingDBData> = db.table("crafting");
+    const craftObj = await table.get(index);
+    const craft = craftingValidate(craftObj?.data, MBS_SLOT_MAX_SERVER + index);
+    if (craft) {
+        logger.debug(`Loading craft ${index + MBS_SLOT_MAX_SERVER} from indexedDB: ${craft.Name} (${craft.Item})`);
+    } else {
+        logger.debug(`Loading empty craft ${index + MBS_SLOT_MAX_SERVER} from indexedDB`);
+    }
+    return craft;
+}
+
+export async function loadAllCraft(db: Dexie): Promise<(null | CraftingItem)[]> {
+    const table: Table<CraftingDBData, number, CraftingDBData> = db.table("crafting");
+    const craftStructs = await table.bulkGet(range(0, MBS_SLOT_MAX_LOCAL - MBS_SLOT_MAX_SERVER));
+    const crafts = craftStructs.map((obj, i) => craftingValidate(obj?.data, MBS_SLOT_MAX_SERVER + i));
+    const nCrafts = crafts.filter(i => i != null).length;
+    logger.debug(`Loading all ${nCrafts} crafts from indexedDB`);
+    return crafts;
+}
---
+++ src/crafting/index.tsx
@@ -0,0 +1,224 @@
+/** Main module for managing all crafting-related additions */
+
+import { inRange } from "lodash";
+import { Dexie } from "dexie";
+
+import { MBS_MOD_API, padArray, logger } from "../common";
+import { waitForBC } from "../common_bc";
+import { pushMBSSettings, SettingsType } from "../settings";
+
+import { openDB, saveCraft, loadAllCraft, saveAllCraft, deleteDB, BC_SLOT_MAX_ORIGINAL, MBS_SLOT_MAX_LOCAL, MBS_SLOT_MAX_SERVER } from "./dexie";
+import styles from "./craft.scss";
+
+export { deleteDB, BC_SLOT_MAX_ORIGINAL, MBS_SLOT_MAX_LOCAL, MBS_SLOT_MAX_SERVER };
+
+/** Serialize the passed crafting items. */
+function craftingSerialize(items: null | readonly (null | CraftingItem)[]): string {
+    if (items == null) {
+        return "";
+    }
+    return items.map(C => C?.Item ? CraftingSerialize(C) : "").join(CraftingSerializeItemSep);
+}
+
+/**
+ * Load crafting items from the MBS cache.
+ * @param character The character in question
+ * @param craftingCache The crafting cache
+ */
+function loadCraftingCache(character: Character, craftingCache: string): void {
+    character.Crafting ??= [];
+    padArray(character.Crafting, BC_SLOT_MAX_ORIGINAL, null);
+    if (!craftingCache) {
+        return;
+    }
+
+    const packet = LZString.compressToUTF16(craftingCache);
+    const data: (null | CraftingItem)[] = CraftingDecompressServerData(packet);
+    let refresh = false;
+    for (const [i, item] of data.entries()) {
+        if (item == null) {
+            continue;
+        } else if (i >= BC_SLOT_MAX_ORIGINAL) {
+            break;
+        }
+
+        // Make sure that the item is a valid craft
+        validate: switch (CraftingValidate(item)) {
+            case CraftingStatusType.OK:
+                break validate;
+            case CraftingStatusType.ERROR:
+                refresh = true;
+                break validate;
+            case CraftingStatusType.CRITICAL_ERROR:
+                logger.error(`Removing corrupt crafting item ${BC_SLOT_MAX_ORIGINAL + i}: "${item?.Name} (${item?.Item})"`);
+                data[i] = null;
+                break validate;
+        }
+    }
+
+    /**
+     * One or more validation errors were encountered that were successfully resolved;
+     * push the fixed items back to the server
+     */
+    if (refresh && character.IsPlayer()) {
+        CraftingSaveServer();
+    }
+}
+
+const IDs = Object.freeze({
+    header: "mbs-crafting-header-prefix",
+    button: "mbs-crafting-info",
+    buttonBrowser: "mbs-crafting-info-browser",
+    buttonServer: "mbs-crafting-info-account",
+    buttonMBS: "mbs-crafting-info-marker",
+    style: "mbs-crafting-styles",
+});
+
+async function loadCraftingNameDOM() {
+    let headingPrefix = document.getElementById(IDs.header);
+    if (!headingPrefix) {
+        headingPrefix = <span id={IDs.header} /> as HTMLSpanElement;
+        const heading = document.querySelector(`#${CraftingID.root} h1`);
+        heading?.prepend(headingPrefix);
+    }
+
+    let infoButton = document.getElementById(IDs.button);
+    if (!infoButton) {
+        infoButton = ElementButton.Create(
+            IDs.button, 
+            () => null,
+            { 
+                image: "./Icons/Question.png",
+                tooltip: [
+                    <span id={IDs.buttonBrowser}>
+                        Extra MBS crafting slot stored locally in your Browser. 
+                        <br />
+                        These crafts are <em>exclusively</em> available for this specific combination of web browser, BC account and BC URL (<code>{window.location.href}</code>). 
+                        As such, it will <em>not</em> be shared between the EU, US and Asia servers.
+                    </span>,
+                    <span id={IDs.buttonServer}>
+                        Extra MBS crafting slot stored on your BC account (the server).
+                        <br />
+                        These crafts are linked to your BC account and will, as such, persists across different web browsers and BC servers.
+                    </span>,
+                ], 
+                tooltipPosition: "left",
+                tooltipRole: "description",
+            },
+            { button: { 
+                attributes: { "aria-label": "Show MBS extra crafting slot description", "aria-disabled": "true" }, 
+                dataAttributes: { type: "account" },
+                children: [<span aria-hidden="true" id={IDs.buttonMBS}>MBS</span>],
+            }},
+        );
+        const menubar = document.getElementById(CraftingID.menuBar);
+        if (menubar) {
+            ElementMenu.AppendButton(menubar, infoButton);
+        }
+    }
+
+    if (CraftingSlot < BC_SLOT_MAX_ORIGINAL) {
+        headingPrefix.replaceChildren();
+        infoButton.hidden = true;
+    } else if (CraftingSlot < MBS_SLOT_MAX_SERVER) {
+        headingPrefix.replaceChildren("MBS (Account): ");
+        infoButton.hidden = false;
+        infoButton.setAttribute("data-type", "account");
+    } else {
+        headingPrefix.replaceChildren("MBS (Browser): ");
+        infoButton.hidden = false;
+        infoButton.setAttribute("data-type", "browser");
+    }
+}
+
+waitForBC("crafting", {
+    async afterLoad() {
+        logger.log("Initializing crafting hooks");
+
+        MBS_MOD_API.patchFunction("CraftingClick", {
+            "if (CraftingOffset < 0) CraftingOffset = 80 - 20;":
+                `if (CraftingOffset < 0) CraftingOffset = ${MBS_SLOT_MAX_LOCAL} - 20;`,
+            "if (CraftingOffset >= 80) CraftingOffset = 0;":
+                `if (CraftingOffset >= ${MBS_SLOT_MAX_LOCAL}) CraftingOffset = 0;`,
+        });
+
+        MBS_MOD_API.patchFunction("CraftingRun", {
+            "/ ${80 / 20}.":
+                `/ ${MBS_SLOT_MAX_LOCAL / 20}.`,
+            'TextGet("SelectDestroy")':
+                `(CraftingOffset >= ${BC_SLOT_MAX_ORIGINAL} ? (CraftingOffset >= ${MBS_SLOT_MAX_SERVER} ? 'MBS (Browser): ' : 'MBS (Account): ') : '') + TextGet("SelectDestroy")`,
+            'TextGet("SelectSlot")':
+                `(CraftingOffset >= ${BC_SLOT_MAX_ORIGINAL} ? (CraftingOffset >= ${MBS_SLOT_MAX_SERVER} ? 'MBS (Browser): ' : 'MBS (Account): ') : '') + TextGet("SelectSlot")`,
+        });
+
+        MBS_MOD_API.hookFunction("CraftingModeSet", 0, ([newMode, ...args], next) => {
+            const ret = next([newMode, ...args]);
+            if (newMode === "Name") {
+                loadCraftingNameDOM();
+            }
+            return ret;
+        });
+    },
+    async afterMBS() {
+        logger.log("Initializing crafting cache");
+
+        let db: Dexie;
+
+        async function loadHook() {
+            if (!document.getElementById(IDs.style)) {
+                document.body.append(<style id={IDs.style}>{styles.toString()}</style>);
+            }
+
+            db = openDB(Player.MemberNumber);
+            padArray(Player.Crafting, MBS_SLOT_MAX_LOCAL, null);
+            for (const [i, craft] of (await loadAllCraft(db)).entries()) {
+                Player.Crafting[i + MBS_SLOT_MAX_SERVER] = craft;
+            }
+        }
+
+        MBS_MOD_API.hookFunction("CraftingLoad", 0, (args, next) => {
+            loadHook();
+            return next(args);
+        });
+
+        // Mirror the extra MBS-specific crafted items to the MBS settings
+        MBS_MOD_API.hookFunction("CraftingSaveServer", 0, (args, next) => {
+            // Only push a single craft to the local storage when it's certain that one is editing a local craft
+            if (inRange(CraftingSlot, MBS_SLOT_MAX_SERVER, MBS_SLOT_MAX_LOCAL)) {
+                const index = CraftingSlot - MBS_SLOT_MAX_SERVER;
+                const craft = Player.Crafting[CraftingSlot];
+                saveCraft(db, index, craft);
+                return;
+            }
+
+            const craftingBackup = Player.Crafting;
+            Player.Crafting = craftingBackup?.slice(0, BC_SLOT_MAX_ORIGINAL);
+            next(args);
+            Player.Crafting = craftingBackup;
+
+            const cache = craftingSerialize(Player.Crafting.slice(BC_SLOT_MAX_ORIGINAL, MBS_SLOT_MAX_SERVER));
+            if (cache != Player.MBSSettings.CraftingCache) {
+                Player.MBSSettings.CraftingCache = cache;
+                pushMBSSettings([SettingsType.SETTINGS]);
+            }
+            
+            // Crafts are synced from within a place of unknown origin; sync the local crafts just in case
+            if (CraftingSlot === 0 && CraftingMode !== "Name") {
+                saveAllCraft(db, Player.Crafting.slice(MBS_SLOT_MAX_SERVER, MBS_SLOT_MAX_LOCAL));
+            }
+        });
+
+        MBS_MOD_API.hookFunction("CraftingExit", 0, (args, next) => {
+            if (CraftingMode === "Slot") {
+                db.close();
+            }
+            return next(args);
+        });
+
+        loadCraftingCache(Player, Player.MBSSettings.CraftingCache);
+
+        if (CurrentScreen === "Crafting") {
+            loadHook();
+        }
+    },
+});
