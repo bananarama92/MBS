2e423ad63223e3cf1a7b51e2691762f195bd0cf9
bananarama92 <bananarama921@outlook.com>
2025-10-28 17:45:02 +0100

    MAINT: Use the BC DOM screen template for the fortune wheel command screen

--- src/fortune_wheel/fortune_wheel_command.tsx
+++ src/fortune_wheel/fortune_wheel_command.tsx
@@ -38,7 +38,7 @@ export class FWCommandScreen extends MBSObjectScreen<FWCommand> {
     readonly settings: FWSelectedCommand;
     static readonly screenParamsDefault = {
         [root]: Object.freeze({
-            shape: [80, 60, 1840, 880] as RectTuple,
+            shape: [60, 40, 1880, 920] as RectTuple,
             visibility: "visible",
         }),
     };
@@ -52,64 +52,95 @@ export class FWCommandScreen extends MBSObjectScreen<FWCommand> {
     ) {
         super(parent, wheelList, index, character, FWCommandScreen.screenParamsDefault, screenParams);
         this.settings = new FWSelectedCommand(wheelList);
-
         const disabled = !character.IsPlayer();
-        document.body.appendChild(
-            <div id={ID.root} class="mbs-screen">
-                <style id={ID.styles}>{styles.toString()}</style>
 
-                <h1 id={ID.header}>{`Customize wheel of fortune command ${this.index}`}</h1>
+        const screen = ElementDOMScreen.getTemplate(
+            ID.root,
+            {
+                parent: document.body,
+                header: `Customize wheel of fortune command ${this.index}`,
+                menubarButtons: [
+                    ElementButton.Create(
+                        ID.exit,
+                        () => this.exit(true, ExitAction.NONE),
+                        { image: "./Icons/Exit.png", tooltip: "Exit", tooltipPosition: "left" },
+                    ),
+                    ElementButton.Create(
+                        ID.cancel,
+                        () => this.exit(false, ExitAction.NONE),
+                        { image: "./Icons/Cancel.png", tooltip: "Cancel", tooltipPosition: "left" },
+                    ),
+                    ElementButton.Create(
+                        ID.accept,
+                        () => this.exit(false, ExitAction.SAVE),
+                        { image: "./Icons/Accept.png", tooltip: "Save command", tooltipPosition: "left" },
+                    ),
+                ],
+                mainContent: [
+                    <fieldset name="main">
+                        <input
+                            type="text"
+                            id={ID.commandInput}
+                            placeholder="Role play command"
+                            disabled={disabled}
+                            maxLength={70}
+                            onInput={(e) => {
+                                this.settings.name = (e.target as HTMLInputElement).value;
+                                const button = document.getElementById(ID.accept) as HTMLButtonElement | null;
+                                button?.toggleAttribute("disabled", this.settings.name.length === 0);
+                            }}
+                            onFocus={(e) => (e.target as HTMLInputElement).select()}
+                        />
 
-                {ElementButton.Create(ID.delete, () => this.exit(false, ExitAction.DELETE), { image: "./Icons/Trash.png", tooltip: "Delete command", tooltipPosition: "right" })}
-                {ElementButton.Create(ID.accept, () => this.exit(false, ExitAction.SAVE), { image: "./Icons/Accept.png", tooltip: "Save command", tooltipPosition: "left" })}
-                {ElementButton.Create(ID.cancel, () => this.exit(false, ExitAction.NONE), { image: "./Icons/Cancel.png", tooltip: "Cancel", tooltipPosition: "left" })}
-                {ElementButton.Create(ID.exit, () => this.exit(true, ExitAction.NONE), { image: "./Icons/Exit.png", tooltip: "Exit", tooltipPosition: "left" })}
+                        <label id={ID.weightHeader} for={ID.weightInput}>Wheel option weight:</label>
+                        <input
+                            type="number"
+                            id={ID.weightInput}
+                            min={1}
+                            max={9}
+                            value={1}
+                            disabled={disabled}
+                            onInput={(e) => {
+                                const target = e.target as HTMLInputElement;
+                                this.settings.weight = clamp(target.valueAsNumber, 1, 9);
+                            }}
+                            onWheel={(e) => {
+                                const target = e.target as HTMLInputElement;
+                                if (target.disabled) {
+                                    return;
+                                }
 
-                <input
-                    type="text"
-                    id={ID.commandInput}
-                    placeholder="Role play command"
-                    disabled={disabled}
-                    maxLength={70}
-                    onInput={(e) => {
-                        this.settings.name = (e.target as HTMLInputElement).value;
-                        const button = document.getElementById(ID.accept) as HTMLButtonElement | null;
-                        button?.toggleAttribute("disabled", this.settings.name.length === 0);
-                    }}
-                    onFocus={(e) => (e.target as HTMLInputElement).select()}
-                />
-
-                <div id={ID.weightHeader}>Wheel option weight:</div>
-                <input
-                    type="number"
-                    id={ID.weightInput}
-                    min={1}
-                    max={9}
-                    value={1}
-                    disabled={disabled}
-                    onInput={(e) => {
-                        const target = e.target as HTMLInputElement;
-                        this.settings.weight = clamp(Number.parseInt(target.value, 10), 1, 9);
-                    }}
-                    onWheel={(e) => {
-                        const target = e.target as HTMLInputElement;
-                        if (target.disabled) {
-                            return;
-                        }
-
-                        if (e.deltaY < 0) {
-                            target.stepUp(1);
-                        } else if (e.deltaY > 0) {
-                            target.stepDown(1);
-                        } else {
-                            return;
-                        }
-                        this.settings.weight = clamp(Number.parseInt(target.value, 10), 1, 9);
-                    }}
-                    onFocus={(e) => (e.target as HTMLInputElement).select()}
-                />
-            </div>,
+                                if (e.deltaY < 0) {
+                                    target.stepUp(1);
+                                } else if (e.deltaY > 0) {
+                                    target.stepDown(1);
+                                } else {
+                                    return;
+                                }
+                                this.settings.weight = clamp(target.valueAsNumber, 1, 9);
+                            }}
+                            onFocus={(e) => (e.target as HTMLInputElement).select()}
+                        />
+                    </fieldset>,
+                ],
+            },
         );
+        screen.append(<style id={ID.styles}>{styles.toString()}</style>);
+        screen.classList.add("mbs-screen");
+
+        const hgroup = screen.querySelector(".screen-hgroup");
+        const header = screen.querySelector(".screen-header");
+        if (hgroup && header) {
+            screen.querySelector("fieldset[name='main']")?.setAttribute("aria-labelledby", hgroup.querySelector("h1")?.id ?? "");
+            header.append(
+                hgroup,
+                ElementButton.Create(
+                    ID.delete,
+                    () => this.exit(false, ExitAction.DELETE),
+                    { image: "./Icons/Trash.png", tooltip: "Delete command", tooltipPosition: "right" },
+                ),
+            );
+        }
     }
 
     async load() {
