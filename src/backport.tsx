35da7446112dd4752a039b44f3216368c2646627
bananarama92 <bananarama921@outlook.com>
2025-11-25 14:51:32 +0100

    MAINT: Backport three bug fixes
    * [BondageProjects/Bondage-College#6033](https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/6033): Fix the color spin buttons in the item color menu failing to switch to the next color
    * [BondageProjects/Bondage-College#6035](https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/6035): Fix the missing entries for the minigame's administration screens
    * [BondageProjects/Bondage-College#6036](https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/6036): Fix opening the color picker resetting layer opacities if the layers have distinct values

--- src/backport.tsx
+++ src/backport.tsx
@@ -20,6 +20,46 @@ waitForBC("backport", {
     async afterLoad() {
         switch (GameVersion) {
             case "R122": {
+                if (
+                    MBS_MOD_API.getOriginalHash("ColorPickerReload") === "E2407218"
+                    && MBS_MOD_API.getOriginalHash("ItemColorLoad") === "2222E0A5"
+                ) {
+                    backportIDs.add(6036);
+                    MBS_MOD_API.patchFunction("ColorPickerReload", {
+                        "ColorPicker.setColor(fieldset, { colorString: singularColor, opacity: singularOpacity }, { overrideEditOpacity: true });":
+                            "ColorPicker.setColor(fieldset, { colorString: singularColor, opacity: singularOpacity }, { overrideEditOpacity: true, dispatch: options.dispatch });",
+                    });
+                    // Workaround for SDK patches being unable to handle functions without an explicit `function` keyword
+                    ColorPicker.setColor = eval(`(() => function ${ColorPicker.setColor.toString()})()`);
+                    MBS_MOD_API.patchFunction("ColorPicker.setColor", {
+                        'fieldset.dispatchEvent(new CustomEvent("input", { detail: { source: outputInput.name } }));':
+                            'if (options.dispatch ?? true) { fieldset.dispatchEvent(new CustomEvent("input", { detail: { source: outputInput.name } })); }',
+                    });
+                    MBS_MOD_API.patchFunction("ItemColorLoad", {
+                        "await ColorPickerInit({ shape });":
+                            "await ColorPickerInit({ shape, dispatch: false });",
+                    });
+                }
+
+                if (MBS_MOD_API.getOriginalHash("ItemColorNextColor") === "518C2598") {
+                    backportIDs.add(6033);
+                    MBS_MOD_API.patchFunction("ItemColorNextColor", {
+                        "const newColors = [...ItemColorState.colors];":
+                            ";",
+                        "colorIndicesToSet.forEach(i => newColors[i] = ItemColorState.defaultColors[i]);":
+                            "colorIndicesToSet.forEach(i => ItemColorItem.Color[i] = ItemColorState.colors[i] = ItemColorState.defaultColors[i]);",
+                        "colorIndicesToSet.forEach(i => newColors[i] = nextColor);":
+                            "colorIndicesToSet.forEach(i => ItemColorItem.Color[i] = ItemColorState.colors[i] = nextColor);",
+                        "ItemColorItem.Color = newColors;":
+                            ";",
+                    });
+                }
+
+                backportIDs.add(6035);
+                ServerPlayerChatRoom.register(
+                    { screen: "GameMagicBattle" },
+                    { screen: "GameClubCard" },
+                );
                 break;
             }
         }
