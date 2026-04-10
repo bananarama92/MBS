25016acac42548dfdb3ed3459cb62d806b258f48
bananarama92 <bananarama921@outlook.com>
2026-04-10 12:07:23 +0200

    MAINT: Drop R125 support

--- src/lpgl/common.ts
+++ src/lpgl/common.ts
@@ -269,7 +269,7 @@ export class Version {
 }
 
 /** The minimum supported BC version. */
-export const BC_MIN_VERSION = 125 satisfies number;
+export const BC_MIN_VERSION = 126 satisfies number;
 
 const bcListenerNames = [
     "api",
