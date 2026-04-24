ec209995be9aca653a3ae9a112de959383322771
bananarama92 <bananarama921@outlook.com>
2026-04-24 12:04:05 +0200

    MAINT: Backport two R128 bug fixes
    * [BondageProjects/Bondage-College#6258](https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/6258): Fix a character variable in `Character.CanChangeClothesOn()`
    * [BondageProjects/Bondage-College#6267](https://gitgud.io/BondageProjects/Bondage-College/-/merge_requests/6267): Ensure that the blindfold flashing queue is only initialized by player characters

--- src/backport.tsx
+++ src/backport.tsx
@@ -16,10 +16,47 @@ const BC_NEXT = BC_MIN_VERSION + 1;
 /** A set with the pull request IDs of all applied bug fix backports */
 export const backportIDs: Set<number> = new Set();
 
+function canChangeClothesOn(this: Character, C: Character): boolean {
+    if (this.IsPlayer() && C.IsPlayer()) {
+        return (
+            !C.IsRestrained() &&
+            !ManagementIsClubSlave() &&
+            OnlineGameAllowChange() &&
+            AsylumGGTSAllowChange(this) &&
+            !LogQuery("BlockChange", "Rule") &&
+            (!LogQuery("BlockChange", "OwnerRule") || !Player.IsFullyOwned())
+        );
+    } else {
+        return (
+            this.CanInteract() &&
+            C.MemberNumber != null &&
+            C.AllowItem &&
+            !C.IsEnclose() &&
+            InventoryGet(C, "ItemNeck")?.Asset.Name !== "ClubSlaveCollar"
+        );
+    }
+}
+
 waitForBC("backport", {
     async afterLoad() {
         switch (GameVersion) {
             case "R127": {
+                if (MBS_MOD_API.getOriginalHash("CharacterCreate") === "3E75642F") {
+                    backportIDs.add(6258);
+                    MBS_MOD_API.hookFunction("CharacterCreate", 0, (args, next) => {
+                        const ret = next(args);
+                        ret.CanChangeClothesOn = canChangeClothesOn;
+                        return ret;
+                    });
+                    Character.forEach(C => C.CanChangeClothesOn = canChangeClothesOn);
+                }
+                if (MBS_MOD_API.getOriginalHash("InventoryRemove") === "1D6A6339") {
+                    backportIDs.add(6267);
+                    MBS_MOD_API.patchFunction("InventoryRemove", {
+                        "BlindFlashQueue = true;":
+                            "if (C.IsPlayer()) { BlindFlashQueue = true; }",
+                    });
+                }
                 if (!document.getElementById("mbs-backport-style")) {
                     document.body.append(<style id="mbs-backport-style">{styles.toString()}</style>);
                 }
